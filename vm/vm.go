16f050b56651d9e430cc18e04b19e9410ad68865
Atharva Thorve <aaathorve@gmail.com>
2025-03-22 01:17:20 -0400

    add Null handling in VM and update tests for logical expressions

--- vm/vm.go
+++ vm/vm.go
@@ -193,6 +193,8 @@ func (vm *VM) executeBangOperator() error {
 		return vm.push(False)
 	case False:
 		return vm.push(True)
+	case Null:
+		return vm.push(True)
 	default:
 		return vm.push(False)
 	}
@@ -240,6 +242,8 @@ func isTruthy(obj object.Object) bool {
 	switch obj := obj.(type) {
 	case *object.Boolean:
 		return obj.Value
+	case *object.Null:
+		return false
 	default:
 		return true
 	}
