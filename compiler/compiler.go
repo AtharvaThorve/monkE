7e0748d4894e413f7b3b10658fae8b4725627797
Atharva Thorve <aaathorve@gmail.com>
2025-03-28 21:23:38 -0400

    add support for hash literals in compiler with corresponding tests

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -5,6 +5,7 @@ import (
 	"monkE/ast"
 	"monkE/code"
 	"monkE/object"
+	"sort"
 )
 
 type SymbolScope string
@@ -237,6 +238,32 @@ func (c *Compiler) Compile(node ast.Node) error {
 			}
 		}
 		c.emit(code.OpArray, len(node.Elements))
+
+	case *ast.HashLiteral:
+		keys := []ast.Expression{}
+		for k := range node.Pairs {
+			keys = append(keys, k)
+		}
+
+		// Here sorting of keys is done so that the tests do not fail.
+		// Otherwise the passing of tests depends on the randomness in which keys are accessed.
+		sort.Slice(keys, func(i, j int) bool {
+			return keys[i].String() < keys[j].String()
+		})
+
+		for _, k := range keys {
+			err := c.Compile(k)
+			if err != nil {
+				return err
+			}
+
+			err = c.Compile(node.Pairs[k])
+			if err != nil {
+				return err
+			}
+		}
+
+		c.emit(code.OpHash, len(node.Pairs)*2)
 	}
 
 	return nil
