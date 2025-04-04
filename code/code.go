5ec6e3c0c366718759e902a84778d5a402dbacdc
Atharva Thorve <aaathorve@gmail.com>
2025-04-04 01:13:40 -0400

    add OpGetLocal and OpSetLocal opcodes with operand handling in VM

--- code/code.go
+++ code/code.go
@@ -34,6 +34,8 @@ const (
 	OpReturnValue
 	// which is similar to code.OpReturnValue, except that there is no explicit value to return but an implicit vm.Null.
 	OpReturn
+	OpGetLocal
+	OpSetLocal
 )
 
 type Opcode byte
@@ -68,6 +70,8 @@ var definitions = map[Opcode]*Definition{
 	OpCall:          {"OpCall", []int{}},
 	OpReturnValue:   {"OpReturnValue", []int{}},
 	OpReturn:        {"OpReturn", []int{}},
+	OpGetLocal:      {"OpGetLocal", []int{1}},
+	OpSetLocal:      {"OpSetLocal", []int{1}},
 }
 
 type Instructions []byte
@@ -138,6 +142,8 @@ func Make(op Opcode, operands ...int) []byte {
 	for i, o := range operands {
 		width := def.OperandWidths[i]
 		switch width {
+		case 1:
+			instruction[offset] = byte(o)
 		case 2:
 			binary.BigEndian.PutUint16(instruction[offset:], uint16(o))
 		}
@@ -153,6 +159,8 @@ func ReadOperands(def *Definition, ins Instructions) ([]int, int) {
 
 	for i, width := range def.OperandWidths {
 		switch width {
+		case 1:
+			operands[i] = int(ReadUint8(ins[offset:]))
 		case 2:
 			operands[i] = int(ReadUint16(ins[offset:]))
 		}
@@ -161,6 +169,8 @@ func ReadOperands(def *Definition, ins Instructions) ([]int, int) {
 
 	return operands, offset
 }
+
+func ReadUint8(ins Instructions) uint8 { return uint8(ins[0]) }
 
 func ReadUint16(ins Instructions) uint16 {
 	return binary.BigEndian.Uint16(ins)
